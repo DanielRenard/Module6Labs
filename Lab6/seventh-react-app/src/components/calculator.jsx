2d06377cd052ec67b806cf0f62efee1a6acacbc6
DanielRenard <danieljuderenard@gmail.com>
2023-12-22 12:48:37 -0600

    finished lab6

--- Lab6/seventh-react-app/src/components/calculator.jsx
+++ Lab6/seventh-react-app/src/components/calculator.jsx
@@ -2,21 +2,54 @@ import { useState } from "react";
 
 function Calculator() {
 
-    const [currentNumber, setCurrentNumber] = useState(number);
+    // const [currentNumber, setCurrentNumber] = useState(number);
 
-    const add = (num1, num2) => {
-        // let num1 = [currentNumber]
-        let value = num1 + num2
-        // setCurrentNumber(value)
-        return value
+    const [operation, setOperation] = useState("+");
+    const [number1, setNumber1] = useState(0);
+    const [number2, setNumber2] = useState(0);
+    const [result, setResult] = useState(0);
+
+    function compute() {  
+        console.log("we in here") 
+        console.log(operation, number1, number2)
+        let computation
+        switch (operation) {
+            case '+':
+                computation = number1 + number2
+                break
+            case '-':
+                computation = number1 - number2
+                break
+            case '*':
+                computation = number1 * number2
+                break
+            case '/':
+                computation = number1 / number2
+                break
+            default:
+                return
+        }
+
+        setResult(computation)
+
     }
-
+    
     return (
+        
         <div>
-            <button onClick={add}>+</button>
+            <input value={number1} type="number" name="number1" onChange={(e) => setNumber1(parseFloat(e.target.value))}/>
+            <select id="operation" value={operation} 
+                onChange={(e) => setOperation(e.target.value)}>
+                    <option>+</option>
+                    <option>-</option>
+                    <option>/</option>
+                    <option>*</option>
+                </select>
+            <input value={number2} type="number" name="number2" onChange={(e) => setNumber2(parseFloat(e.target.value))}/>
+            <button id="submit" onClick={() => {compute()}}>Submit</button>
+            <div id="result">{result}</div>
         </div>
     )
-
 }
 
 export default Calculator
